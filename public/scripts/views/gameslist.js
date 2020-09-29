7399b676351435b6362b22b4880e6b688b3b0e8d
reedling78 <reedling78@gmail.com>
2020-09-29 17:23:13 -0500

    Switch to react

--- public/scripts/views/gameslist.js
+++
@@ -1,9 +0,0 @@
-/*jslint nomen: true*/
-/*globals window, document, define, $, _, console, alert, app*/
-
-define(['hbs!templates/gameslist'], function (hbsGamesList) {
-    'use strict';
-
-    app.$content.html(hbsGamesList({}));
-
-});
